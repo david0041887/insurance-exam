1ae4ec6d28e8de971d89b54b9f916e5e2a0d250b
david0041887 <107393233+david0041887@users.noreply.github.com>
2026-05-21 18:17:17 +0800

    Bump SW cache to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ins-exam-v8';
+const CACHE = 'ins-exam-v9';
 const ASSETS = ['./', './index.html', './questions.json', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
